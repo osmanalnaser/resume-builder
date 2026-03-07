01c247efb06a162d1aaa66c0b21f6b3d99929dce
osman <osman.n2@icloud.com>
2026-03-07 17:58:11 +0100

    feat: add dashboard with resume grid, create and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )
