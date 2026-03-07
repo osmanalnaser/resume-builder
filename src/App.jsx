a5fd3e380fcddae304c506ec9a90c83c463d24c8
osman <osman.n2@icloud.com>
2026-03-07 18:05:03 +0100

    feat: add resume editor with live preview, experience, education and skills sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import Dashboard from './pages/Dashboard'
+import ResumeEditor from './pages/ResumeEditor'
 import ProtectedRoute from './components/ProtectedRoute'
+
 
 function App() {
   return (
@@ -9,9 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
+          <ProtectedRoute><Dashboard /></ProtectedRoute>
+        } />
+        <Route path="/resume/:id" element={
+          <ProtectedRoute><ResumeEditor /></ProtectedRoute>
         } />
       </Routes>
     </BrowserRouter>
